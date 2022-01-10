2abc71cc3596c5a492b4b27580b3d4d96564dbba
Md. Rifaet Ullah <rifaet.ullah@mymedicalhub.com>
2022-01-10 20:40:52 +0600

    (DEV) Added exercise view model file

---
+++ app/src/main/java/com/mymedicalhub/emmavirtualtherapist/android/feature_exercise/presentation/ExerciseViewModel.kt
@@ -0,0 +1,8 @@
+package com.mymedicalhub.emmavirtualtherapist.android.feature_exercise.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ExerciseViewModel : ViewModel() {
+}
